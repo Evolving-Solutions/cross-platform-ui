46dc53dbf03f5c048173528684795a827da1ef5f
Kato Charles <kcharles52@gmail.com>
2022-06-28 17:29:13 +0300

    create app palette

---
+++ lib/config/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// app color palette
+class Palette {
+  static Color primaryTeal = const Color.fromRGBO(0, 183, 195, 1);
+  static Color primaryCasal = const Color.fromRGBO(47, 93, 102, 1);
+  static Color secondaryCoral = const Color.fromRGBO(254, 111, 97, 1);
+  static Color secondaryGranite = const Color.fromRGBO(94, 94, 94, 1);
+}
