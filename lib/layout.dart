7916307b9cf831ae5f546cbd1bb91d7189c20e95
Kato Charles <kcharles52@gmail.com>
2022-06-30 17:28:48 +0300

    use respinsive layout

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:es_ur_shortener/config/palette.dart';
 import 'package:es_ur_shortener/widgets/large_screen.dart';
 import 'package:es_ur_shortener/widgets/small_screen.dart';
 import 'package:es_ur_shortener/helpers/responsiveness.dart';
+import 'package:es_ur_shortener/widgets/top_navigation.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  SiteLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Palette.secondaryGranite,
-      ),
-      body: const ResponsiveWidget(largeScreen: LargeScreen(), mediumScreen: LargeScreen(), smallScreen: SmallScreen(), customScreen: LargeScreen())
-    );
+        appBar: topNavigationBar(context, scaffoldKey),
+        body: const ResponsiveWidget(
+            largeScreen: LargeScreen(),
+            mediumScreen: LargeScreen(),
+            smallScreen: SmallScreen(),
+            customScreen: LargeScreen()));
   }
 }
