025e9e82c383f1986155a5d4f5798239fb4efc7b
Kato Charles <kcharles52@gmail.com>
2022-06-30 17:29:02 +0300

    use site layout

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         }),
         primaryColor: Palette.primaryCasal,
       ),
-      home: const SiteLayout(),
+      home: SiteLayout(),
     );
   }
 }
