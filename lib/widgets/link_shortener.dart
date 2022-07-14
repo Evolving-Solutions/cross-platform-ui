fd0f3a64c97d38e023dd0472578cea5c6c54ea1b
Kato Charles <kcharles52@gmail.com>
2022-07-14 17:51:54 +0300

    create url shortner function

--- lib/widgets/link_shortener.dart
+++ lib/widgets/link_shortener.dart
@@ -1,6 +1,7 @@
-import 'package:es_ur_shortener/config/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:es_ur_shortener/config/palette.dart';
+import 'package:es_ur_shortener/services/remote_service.dart';
 
 class LinkShortenerContainer extends StatefulWidget {
   const LinkShortenerContainer({Key? key}) : super(key: key);
@@ -12,6 +13,15 @@ class LinkShortenerContainer extends StatefulWidget {
 class _LinkShortenerContainerState extends State<LinkShortenerContainer> {
   final _formKey = GlobalKey<FormState>();
 
+  var isLoaded = false;
+  String? shortenedUrl;
+
+  // shortenUrl
+  handleShortenUrl(String url) async {
+    shortenedUrl = await RemoteService().shortenUrl(url);
+    // print('shortenedUrl ${shortenedUrl}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,89 +30,109 @@ class _LinkShortenerContainerState extends State<LinkShortenerContainer> {
       width: double.infinity,
       // padding: EdgeInsets.all(value),
       // decoration: DecoratedBox(decoration: decoration),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 600,
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Make your links shorter',
-                          style: GoogleFonts.raleway(
-                            textStyle: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 40,
-                                fontWeight: FontWeight.w700),
-                          )),
-                      Text(
-                        'Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.',
-                        style: GoogleFonts.raleway(
-                          textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w400),
-                        ),
-                        textAlign: TextAlign.center,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 600,
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Make your links shorter',
+                              style: GoogleFonts.raleway(
+                                textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w700),
+                              )),
+                          Text(
+                            'Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.',
+                            style: GoogleFonts.raleway(
+                              textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                )
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 630,
+                      height: 50,
+                      color: Colors.white,
+                      // padding: const EdgeInsets.all(20),
+                      margin: const EdgeInsets.all(20),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'URL',
+                            contentPadding: EdgeInsets.all(10.0)),
+                        onSaved: (String? value) {
+                          // print(value);
+                          // call api
+                          handleShortenUrl(value!);
+                        },
+                        validator: (String? value) {
+                          return (value != null && value.contains('@'))
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 140,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState?.save();
+                              // handleShortenUrl();
+                            }
+                          },
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Palette.secondaryCoral)),
+                          child: const Text(
+                            'Shorten URL',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    )
+                  ],
+                ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 630,
-                  height: 50,
-                  color: Colors.white,
-                  // padding: const EdgeInsets.all(20),
-                  margin: const EdgeInsets.all(20),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'URL',
-                        contentPadding: EdgeInsets.all(10.0)),
-                    onSaved: (String? value) {
-                      print(value);
-                      // call api
-                    },
-                    validator: (String? value) {
-                      return (value != null && value.contains('@'))
-                          ? 'Do not use the @ char.'
-                          : null;
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  width: 140,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState?.save();
-                      }
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Palette.secondaryCoral)),
-                    child: const Text(
-                      'Shorten URL',
-                      style: TextStyle(color: Colors.white),
-                    )),
-                )
-              ],
-            )
-          ],
-        ),
+          ),
+          Visibility(
+            visible: isLoaded,
+            replacement: const Center(child: CircularProgressIndicator()),
+            child: SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  return Container(child: const Text('Hello'));
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
